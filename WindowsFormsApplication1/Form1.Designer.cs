b221dbcdc37131360a146b93269c1096808bebe0
yuheng.lin <yuheng.lin@hand-china.com>
2017-01-09 18:01:33 +0800

    添加项目文件。

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.audioText = new System.Windows.Forms.TextBox();
+            this.videoText = new System.Windows.Forms.TextBox();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.extension = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // audioText
+            // 
+            this.audioText.Location = new System.Drawing.Point(23, 27);
+            this.audioText.Margin = new System.Windows.Forms.Padding(2);
+            this.audioText.Name = "audioText";
+            this.audioText.ReadOnly = true;
+            this.audioText.Size = new System.Drawing.Size(162, 21);
+            this.audioText.TabIndex = 0;
+            this.audioText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // videoText
+            // 
+            this.videoText.Location = new System.Drawing.Point(23, 81);
+            this.videoText.Margin = new System.Windows.Forms.Padding(2);
+            this.videoText.Name = "videoText";
+            this.videoText.ReadOnly = true;
+            this.videoText.Size = new System.Drawing.Size(162, 21);
+            this.videoText.TabIndex = 1;
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(23, 130);
+            this.outputText.Margin = new System.Windows.Forms.Padding(2);
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(162, 21);
+            this.outputText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "音频";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "视频";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 115);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "输出文件";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 166);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 18);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "合并";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 28);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 18);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "打开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(202, 82);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 18);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "打开";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // extension
+            // 
+            this.extension.AutoSize = true;
+            this.extension.Location = new System.Drawing.Point(190, 133);
+            this.extension.MinimumSize = new System.Drawing.Size(10, 0);
+            this.extension.Name = "extension";
+            this.extension.Size = new System.Drawing.Size(10, 12);
+            this.extension.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 202);
+            this.Controls.Add(this.extension);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.videoText);
+            this.Controls.Add(this.audioText);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "流媒体工厂";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox audioText;
+        private System.Windows.Forms.TextBox videoText;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label extension;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CombineAudioAndVideo();
+        }
+
+        private void CombineAudioAndVideo()
+        {
+            String audioTextStr = audioText.Text;
+            String videoTextStr = videoText.Text;
+            String outputTextStr = outputText.Text + extension.Text;
+            //String outputTextStr = "SECHSKIES - '컴백(COM' BACK)'   '세 단어(THREE WORDS)'   '커플(COUPLE)' in 2016 MELON MUSIC AWARDS.mp4";
+            Process p = new Process();//创建进程对象 
+            p.StartInfo.FileName = "cmd.exe";//设定需要执行的命令 
+                                             // startInfo.Arguments = "/C " + command;//“/C”表示执行完命令后马上退出  
+            p.StartInfo.UseShellExecute = false;//不使用系统外壳程序启动 
+            p.StartInfo.RedirectStandardInput = true;//可以重定向输入,接受来自调用程序的输入信息  
+            p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不创建窗口
+            //p.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+            //p.StartInfo.StandardErrorEncoding = Encoding.UTF8;
+            p.Start();//启动程序
+            Console.WriteLine(p.StandardInput.Encoding.EncodingName);
+            Console.WriteLine(Encoding.GetEncoding(949).EncodingName);
+            p.StandardInput.WriteLine("D:");
+            p.StandardInput.WriteLine(@"cd D:\\win-youtube-dl");
+            //p.StandardInput.WriteLine(@"ping 192.168.123.11");
+            
+            p.StandardInput.WriteLine(@"ffmpegg -i """+audioTextStr+"\" -i \""+videoTextStr+"\" -vcodec copy -acodec copy \""+ outputTextStr+"\"");
+            p.StandardInput.WriteLine(@"exit");
+            //获取cmd窗口的输出信息
+            //p.StandardInput.AutoFlush = true;
+
+            //--------目前有阻塞问题，待解决 BEGIN----------
+            string output = p.StandardOutput.ReadToEnd();
+            //string error = p.StandardError.ReadToEnd();
+            //p.WaitForExit();//等待程序执行完退出进程
+            Console.WriteLine(output);
+            //Console.WriteLine(error);
+            //--------目前有阻塞问题，待解决 END----------
+            if (p.HasExited)
+            {
+            p.Close();
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "音频文件(*.m4a)|*.m4a|所有文件|*.*";
+            ofd.ValidateNames = true;
+            ofd.CheckPathExists = true;
+            ofd.CheckFileExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                audioText.Text = ofd.FileName;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "视频文件(*.mp4,*.avi,*.mkv)|*.mp4;*.avi;*.mkv|所有文件|*.*";
+            ofd.ValidateNames = true;
+            ofd.CheckPathExists = true;
+            ofd.CheckFileExists = true;
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                videoText.Text = ofd.FileName;
+                outputText.Text = Path.GetFileNameWithoutExtension(ofd.FileName);
+                extension.Text = Path.GetExtension(ofd.FileName);
+            }
+        }
+
+        public static string get_uft8(string unicodeString)
+        {
+            UTF8Encoding utf8 = new UTF8Encoding();
+            Byte[] encodedBytes = utf8.GetBytes(unicodeString);
+            String decodedString = utf8.GetString(encodedBytes);
+            return decodedString;
+        }
+    }
+}
